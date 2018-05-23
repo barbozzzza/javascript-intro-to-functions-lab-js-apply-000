0634ebc40f7793138bd8d5d8e6c357b34a277ba6
jose barboza <jrbarboza08@gmail.com>
2018-05-23 20:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function logWhisper(String){
 
 function sayHiToGrandma(String){
 
+if () {
+    block of code to be executed if the condition is true
+} else { 
+    block of code to be executed if the condition is false
+}
+
 
 }
 
